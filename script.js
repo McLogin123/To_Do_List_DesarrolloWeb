5076e0c4173da721766cc4d3279b636563a5fbe9
McLogin123 <103536901+McLogin123@users.noreply.github.com>
2025-08-31 20:17:03 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 const BotonAgregar = document.getElementById('agregar');
 const NotaInput = document.getElementById('lista-input');
 const NotasContainer = document.getElementById('lista-tareas');
+const NotaLista = document.getElementsByClassName('nota');
 
 BotonAgregar.addEventListener('click', () => {
     const NotaText = NotaInput.value;
     if (NotaText.trim() === '') {
         alert("Nota vacia, por favor ingrese una tarea");
     } else {
-        const NotaDiv = document.createElement('div');
-        NotaDiv.className = 'note';
+        const NotaDiv = document.createElement('button');
+        NotaDiv.className = 'nota';
         NotaDiv.textContent = NotaText;
         NotasContainer.appendChild(NotaDiv)
         NotaInput.value = '';
